6be8e6fa9a815df0f95dbe1e63a662202bb79904
Valian <mgutiers3012@gmail.com>
2020-06-01 11:37:41 -0400

    comenzando

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,28 @@
-const express = require('express')
-const router = express.Router()
+const express = require("express");
+const router = express.Router();
 
+router.post("github", () => {
+  try {
+    exec(
+      `cd ${__dirname} && git pull origin master`,
+      (error, stdout, stderr) => {
+        if (error) {
+          console.log(`error: ${error.message}`);
+          return;
+        }
+        if (stderr) {
+          console.log(`stderr: ${stderr}`);
+          return;
+        }
+        console.log(`stdout: ${stdout}`);
+      }
+    );
 
+    return res.status(200).json("ok");
+  } catch (error) {
+    console.log(error);
+    return res.status(500);
+  }
+});
 
-module.exports = router
+module.exports = router;
